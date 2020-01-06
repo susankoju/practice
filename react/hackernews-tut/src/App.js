c1d26788116634df0e57af5e1f5216c3b6b7e00a
susankoju <you@example.com>
2020-01-06 10:45:14 +0545

    adding

---
+++ react/hackernews-tut/src/App.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+
+export const App = () => <p>Hello World!</p>
---
+++ react/hackernews-tut/src/services/hnApi.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+export const newStoriesUrl = `${baseUrl}newstories`;
+export const storyUrl = `${baseUrl}item/`;
