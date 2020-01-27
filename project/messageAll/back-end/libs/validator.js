19bb574fad86b54d97d994b5a74a462e07bc581a
susankoju <aksusankoju@gmail.com>
2020-01-27 17:49:21 +0545

    server side validation for signin and signup

--- project/messageAll/back-end/libs/validator.js
+++ project/messageAll/back-end/libs/validator.js
@@ -1,27 +1,80 @@
 const JOI = require('@hapi/joi');
 
-userValid =(input)=>{
+userSignupValid =(input)=>{
 
     const schema = JOI.object({
-        username: JOI.string()
-            .min(3)
-            .max(30)
+        email: JOI.string()
+            .email()
             .required(),
+
+        password: JOI.string()
+            .min(8)
+            .max(35)
+            .required(),
+
+        firstName: JOI.string()
+            .regex(/^[a-zA-Z*]+$/)
+            .min(2)
+            .max(50)
+            .required(),
+
+        lastName: JOI.string()
+            .min(2)
+            .max(50)
+            .regex(/^[a-zA-Z*]+$/)
+            .required(),
+
+        image: JOI.string()
+            .allow('',null),
+
+        address: JOI.string()
+            .required()
+            .min(3)
+            .max(50),
+
+        contact: JOI.string()
+            .min(5)
+            .max(20),
+
+        role: JOI.string()
+            .allow('', null),
     });
     
 
-return new Promise=(resolve,reject) => {
+    return new Promise((resolve,reject) => {
 
         const { error, value } = schema.validate(input);
         if (error)
             reject(error);
         else
             resolve(value);
-}
+    });
 
     //validation
 };
 
+
+userSigninValid = (data) => {
+    const schema = JOI.object({
+        email: JOI.string()
+            .email()
+            .required(),
+
+        password: JOI.string()
+            .min(8)
+            .max(35)
+            .required(),
+
+    });
+
+    return new Promise ((resolve, reject) => {
+        const {error, value} = schema.validate(data);
+        if(error) reject(error);
+        else resolve(value);
+    });
+}
+
 module.exports={
-    userValid,
+    userSignupValid,
+    userSigninValid
 };
--- project/messageAll/back-end/modules/user/user.controller.js
+++ project/messageAll/back-end/modules/user/user.controller.js
@@ -2,34 +2,46 @@ const query = require('./user.query');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const jwtSecret = require('../../configs/index').jwtSecret;
+const {userSigninValid, userSignupValid} = require('../../libs/validator');
 
 module.exports = {
     signup: (req, res, next) => {
-        bcrypt.hash(req.body.password, 10)
-            .then((hash) => {
-                const data = {
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    address: req.body.address,
-                    contact: req.body.contact,
-                    email: req.body.email,
-                    password: hash,
-                    image: req.body.image,
-                    role: 'member'
-                }
-                query.insert(data)
-                    .then((result) => {
-                        res.send(result);
+        userSignupValid(req.body)
+            .then(result => {
+
+                bcrypt.hash(req.body.password, 10)
+                    .then((hash) => {
+                        const data = {
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            address: req.body.address,
+                            contact: req.body.contact,
+                            email: req.body.email,
+                            password: hash,
+                            image: req.body.image,
+                            role: 'member'
+                        }
+                        query.insert(data)
+                            .then((result) => {
+                                res.send(result);
+                            })
+                            .catch((err) => {
+                                next({
+                                    err: err
+                                })
+                            })
                     })
                     .catch((err) => {
-                        next({
-                            err: err
-                        })
+                        next({ err: err })
                     })
+
             })
-            .catch((err) => {
-                next({ err: err })
+            .catch(error => {
+                next({
+                    err: error
+                })
             })
+
     },
     profile: (req, res, next) => {
         query.selectById(req.params.id)
@@ -46,32 +58,41 @@ module.exports = {
     },
 
     signin : (req, res, next) => {
-        query.selectByEmail(req.body.email)
-        .then(result=>{
-            
-            bcrypt.compare(req.body.password, result[0].password, (err, match) => {
- 
-                if(err) res.send(err);
-                else if (match) {
-                    res.json({
-                        'token': jwt.sign({ 'email': req.body.email }, jwtSecret),
-                        'id': result[0].id,
-                        'firstName': result[0].firstName,
-                        'lastName': result[0].lastName,
-                    });
-                }
-                else {
-                    next({
-                        err: "Wrong Username or Password!"
-                    });
-                }
-            })            
-        })
-        .catch((err)=>{
-            next({
-                err: err
-            });
-        })
+        userSigninValid({'email': req.body.email, 'password': req.body.password})
+            .then(result => {
+                query.selectByEmail(req.body.email)
+                    .then(result => {
+
+                        bcrypt.compare(req.body.password, result[0].password, (err, match) => {
+
+                            if (err) res.send(err);
+                            else if (match) {
+                                res.json({
+                                    'token': jwt.sign({ 'email': req.body.email }, jwtSecret),
+                                    'id': result[0].id,
+                                    'firstName': result[0].firstName,
+                                    'lastName': result[0].lastName,
+                                });
+                            }
+                            else {
+                                next({
+                                    err: "Wrong Username or Password!"
+                                });
+                            }
+                        })
+                    })
+                    .catch((err) => {
+                        next({
+                            err: err
+                        });
+                    })
+            })
+            .catch(error => {
+                next({
+                    err: error
+                })
+            })
+
 
     },
 
